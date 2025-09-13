2d4800b49de4495152f34237715cde4f7f7ce6db
vandersonmgpb <vanderson1022@gmail.com>
2025-09-13 16:53:56 -0300

    trabalhando com bilbioteca faker

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
+const { faker } = require('@faker-js/faker')
 
 const { LandingPage } = require('../pages/LandingPage')
 const { Toast } = require('../pages/Components')
@@ -13,9 +14,13 @@ test.beforeEach(async ({ page }) => {
 })
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
+
+  const leadName = faker.person.fullName()
+  const leadEmail = faker.internet.email()
+
   await landingPage.visit()
   await landingPage.openLeadModal()
-  await landingPage.submitLeadForm('Fulano de Tall', 'fulanodetall@email.com')
+  await landingPage.submitLeadForm(leadName, leadEmail)
 
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
   await toast.haveText(message)
