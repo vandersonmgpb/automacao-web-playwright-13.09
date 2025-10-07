ed6cadb2aba396a65ecbc762c7f2018c9f6d5e8f
vandersonmgpb <vanderson1022@gmail.com>
2025-10-07 10:10:08 -0300

    mutiple reporters

--- tests/support/database.js
+++ tests/support/database.js
@@ -22,7 +22,7 @@ export async function executeSQL(sqlScript) {
         const client = await pool.connect()
 
         const result = client.query(sqlScript)
-        console.log(result.rows)
+        // console.log(result.rows)
     } catch (error) {
         console.log('Erro ao executar SQL ' + error)
     }
