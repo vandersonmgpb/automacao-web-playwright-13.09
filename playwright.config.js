3fdfac6862ae3fa3f8ef7a3c76974fdd1da3b902
vandersonmgpb <vanderson1022@gmail.com>
2025-10-07 10:33:00 -0300

    reports ricos na nuvem, utilizando o Tesults

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['dot'],
-    ['html']
+    ['playwright-tesults-reporter', {'tesults-target': process.env.TOKEN}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
